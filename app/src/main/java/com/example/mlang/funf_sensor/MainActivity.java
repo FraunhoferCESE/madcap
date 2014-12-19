250ab9a995a1d8179bf4d023c757c8a7a361cb24
mlang <mlang@fc-md.umd.edu>
2014-12-19 14:08:03 -0500

    created basic layout including toggle button for data collection, save data to sc card button, save data to remote database now and a small text.

---
+++ app/src/main/java/com/example/mlang/funf_sensor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mlang.funf_sensor;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+/**
+ * Created by MLang on 19.12.2014.
+ */
+public class MainActivity extends Activity{}
