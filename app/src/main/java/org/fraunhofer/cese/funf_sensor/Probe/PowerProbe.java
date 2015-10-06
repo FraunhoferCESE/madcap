649c3d521dfee932df91f11636d4b0e03f574eff
fdietrich <fdietrich@fc-md.umd.edu>
2015-10-06 15:44:22 -0400

    SMSProbe now sending new Intent with timestamp and added infos only. PowerProbe now reacting to actual changes in power level only

--- app/src/main/java/org/fraunhofer/cese/funf_sensor/Probe/PowerProbe.java
+++ app/src/main/java/org/fraunhofer/cese/funf_sensor/Probe/PowerProbe.java
@@ -10,16 +10,15 @@ import android.util.Log;
 import edu.mit.media.funf.probe.Probe;
 
 /**
- *PowerProbe can tell the current battery level and if it's charging right now
+ * PowerProbe can tell the current battery level and if it's charging right now
  */
 public class PowerProbe extends Probe.Base implements Probe.PassiveProbe {
-
-    private static final String TAG = "PowerProbe";
 
     /**
      * the internal class PowerInform... is on the bottom of this class
      */
     private BroadcastReceiver receiver;
+    private static int powerLevel=0;
 
     @Override
     protected void onEnable() {
@@ -28,9 +27,9 @@ public class PowerProbe extends Probe.Base implements Probe.PassiveProbe {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.intent.action.ACTION_POWER_CONNECTED");
         intentFilter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
-        intentFilter.addAction("android.intent.action.ACTION_BATTERY_LOW");
-        intentFilter.addAction("android.intent.action.ACTION_BATTERY_OKAY");
-        intentFilter.addAction("android.intent.action.ACTION_BATTERY_CHANGED");
+        intentFilter.addAction("android.intent.action.BATTERY_LOW");
+        intentFilter.addAction("android.intent.action.BATTERY_OKAY");
+        intentFilter.addAction("android.intent.action.BATTERY_CHANGED");
         getContext().registerReceiver(receiver, intentFilter);
         Log.i("PowerProbe.class: ", "PowerProbe enabled");
 
@@ -51,59 +50,111 @@ public class PowerProbe extends Probe.Base implements Probe.PassiveProbe {
     public class PowerInformationReceiver extends BroadcastReceiver {
 
         public PowerProbe callback;
-        public PowerInformationReceiver (PowerProbe callback){this.callback=callback;}
+
+        public PowerInformationReceiver(PowerProbe callback) {
+            this.callback = callback;
+        }
 
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            intent = addChargingStatus(intent);
-            intent = addPlugStatus(intent);
-            intent = addBatteryLevel(intent);
+            switch (intent.getAction()) {
 
-            callback.sendData(intent);
+                case Intent.ACTION_POWER_CONNECTED:
+
+                    String plug = "";
+                    int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                    if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
+                        plug = "AC";
+                    }else if(chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
+                        plug = "USB";
+                    }
+                    intent.putExtra("PowerProbe: ", "now connected to" + plug);
+                    callback.sendData(intent);
+                    break;
+
+                case Intent.ACTION_POWER_DISCONNECTED:
+
+                    intent.putExtra("PowerProbe: ", "now disconnected");
+                    callback.sendData(intent);
+                    break;
+
+                case Intent.ACTION_BATTERY_LOW:
+
+                    intent.putExtra("PowerProbe: ", "Battery low!");
+                    callback.sendData(intent);
+                    break;
+
+                case Intent.ACTION_BATTERY_OKAY:
+
+                    intent.putExtra("PowerProbe: ", "Battery okay.");
+                    callback.sendData(intent);
+                    break;
+
+                case Intent.ACTION_BATTERY_CHANGED:
+
+                    if((intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1))!=powerLevel){
+                        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                        powerLevel=level;
+                        float batteryPct = level;
+                        intent.putExtra("PowerProbe: ", "Battery at " + batteryPct +" %");
+                        callback.sendData(intent);
+                    }
+                    break;
+
+                default:
+
+                    intent.putExtra("PowerProbe: ","Something went wrong.");
+                    callback.sendData(intent);
+                    break;
+            }
+
+//            intent = addChargingStatus(intent);
+//            intent = addPlugStatus(intent);
+//            intent = addBatteryLevel(intent);
+
+//            callback.sendData(intent);
 
 
         }
 
-        private Intent addChargingStatus(Intent intent) {
-
-            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-
-            if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
-                intent.putExtra("Battery Status: ", "Charging");
-            }
-
-            if (status == BatteryManager.BATTERY_STATUS_FULL) {
-                intent.putExtra("Battery Status: ", "Full");
-            }
-
-            return intent;
-        }
-
-        private Intent addPlugStatus(Intent intent) {
-
-            int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-            if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
-                intent.putExtra("Plugged to: ", "AC");
-            }
-
-            if (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
-                intent.putExtra("Plugged to: ", "USB");
-            }
-
-            return intent;
-        }
-
-        private Intent addBatteryLevel(Intent intent) {
-
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-
-            float batteryPct = level / (float) scale;
-
-            intent.putExtra("Battery Status: ", batteryPct + "%");
-
-            return intent;
-        }
+//        private Intent addChargingStatus(Intent intent) {
+//
+//            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+//
+//            if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
+//                intent.putExtra("Battery Status: ", "Charging");
+//            }
+//
+//            if (status == BatteryManager.BATTERY_STATUS_FULL) {
+//                intent.putExtra("Battery Status: ", "Full");
+//            }
+//
+//            return intent;
+//        }
+//
+//        private Intent addPlugStatus(Intent intent) {
+//
+//            int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+//            if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
+//                intent.putExtra("Plugged to: ", "AC");
+//            }
+//
+//            if (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
+//                intent.putExtra("Plugged to: ", "USB");
+//            }
+//
+//            return intent;
+//        }
+//
+//        private Intent addBatteryLevel(Intent intent) {
+//
+//            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+//            float batteryPct = level;
+//
+//            intent.putExtra("Battery Status: ", batteryPct +" %");
+//
+//            return intent;
+//        }
     }
 }
