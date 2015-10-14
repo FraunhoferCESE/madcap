779be1eeec7928c832d2cbfe11932e48209e6ad4
fdietrich <fdietrich@fc-md.umd.edu>
2015-10-14 15:03:58 -0400

    1

---
+++ app/src/main/java/org/fraunhofer/cese/funf_sensor/AutoStart.java
@@ -0,0 +1,20 @@
+package org.fraunhofer.cese.funf_sensor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+/**
+ *
+ */
+public class AutoStart extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent startIntent = new Intent(context, MainActivity.class);
+        context.startService(startIntent);
+        Log.i("AutoStart: ", "launched!!");
+    }
+}
