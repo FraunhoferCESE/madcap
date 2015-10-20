6a41e80cb9339ab35125ab3c139cb4ad56cdc9ce
Lucas Layman <llayman@fc-md.umd.edu>
2015-10-20 16:55:17 -0400

    merged remoteTesting and changed configuration for emulator

--- app/src/main/java/org/fraunhofer/cese/funf_sensor/MyModule.java
+++ app/src/main/java/org/fraunhofer/cese/funf_sensor/MyModule.java
@@ -2,6 +2,8 @@ package org.fraunhofer.cese.funf_sensor;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 
@@ -11,6 +13,8 @@ import org.fraunhofer.cese.funf_sensor.cache.Cache;
 import org.fraunhofer.cese.funf_sensor.cache.CacheConfig;
 import org.fraunhofer.cese.funf_sensor.cache.DatabaseAsyncTaskFactory;
 import org.fraunhofer.cese.funf_sensor.cache.RemoteUploadAsyncTaskFactory;
+
+import java.io.IOException;
 
 /**
  * Created by Lucas on 10/6/2015.
@@ -23,39 +27,39 @@ public class MyModule extends AbstractModule {
         binder.bind(GoogleAppEnginePipeline.class);
     }
 
-//    @Provides
-//    CacheConfig provideCacheConfig() {
-//        CacheConfig config = new CacheConfig();
-//        config.setMaxMemEntries(2);
-//        config.setMaxDbEntries(5);
-//
-//        config.setMemForcedCleanupLimit(5000);
-//        config.setDbForcedCleanupLimit(30000); // Required to make sure upload doesn't exceed Google API limits for a single request
-//
-//        config.setDbWriteInterval(1000); // 2 second minimum
-//        config.setUploadInterval(5000); // 15 minute minimum
-//
-//        config.setUploadWifiOnly(true);
-//
-//        return config;
-//    }
-
     @Provides
     CacheConfig provideCacheConfig() {
         CacheConfig config = new CacheConfig();
-        config.setMaxMemEntries(100);
-        config.setMaxDbEntries(250);
+        config.setMaxMemEntries(2);
+        config.setMaxDbEntries(5);
 
         config.setMemForcedCleanupLimit(5000);
         config.setDbForcedCleanupLimit(30000); // Required to make sure upload doesn't exceed Google API limits for a single request
 
-        config.setDbWriteInterval(2000); // 2 second minimum
-        config.setUploadInterval(75000); // 15 minute minimum
+        config.setDbWriteInterval(1000); // 2 second minimum
+        config.setUploadInterval(5000); // 15 minute minimum
 
         config.setUploadWifiOnly(true);
 
         return config;
     }
+
+//    @Provides
+//    CacheConfig provideCacheConfig() {
+//        CacheConfig config = new CacheConfig();
+//        config.setMaxMemEntries(100);
+//        config.setMaxDbEntries(250);
+//
+//        config.setMemForcedCleanupLimit(5000);
+//        config.setDbForcedCleanupLimit(30000); // Required to make sure upload doesn't exceed Google API limits for a single request
+//
+//        config.setDbWriteInterval(2000); // 2 second minimum
+//        config.setUploadInterval(75000); // 15 minute minimum
+//
+//        config.setUploadWifiOnly(true);
+//
+//        return config;
+//    }
 
     @Provides
     DatabaseAsyncTaskFactory provideDatabaseWriteAsyncTaskFactory() {
@@ -67,33 +71,33 @@ public class MyModule extends AbstractModule {
         return new RemoteUploadAsyncTaskFactory();
     }
 
-//    @Provides
-//    ProbeDataSetApi provideProbeDataSetApi() {
-//        ProbeDataSetApi.Builder builder = new ProbeDataSetApi.Builder(AndroidHttp.newCompatibleTransport(),
-//                new AndroidJsonFactory(), null)
-//                .setApplicationName("funfSensor")
-//                .setRootUrl("http://192.168.0.100:8080/_ah/api/")
-//                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-//                    @Override
-//                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-//                        abstractGoogleClientRequest.setDisableGZipContent(true);
-//                    }
-//                });
-//        return builder.build();
-//    }
-
     @Provides
     ProbeDataSetApi provideProbeDataSetApi() {
         ProbeDataSetApi.Builder builder = new ProbeDataSetApi.Builder(AndroidHttp.newCompatibleTransport(),
                 new AndroidJsonFactory(), null)
-                .setRootUrl("https://funfcese.appspot.com/_ah/api/");
-//                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-//                    @Override
-//                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-//                        abstractGoogleClientRequest.setDisableGZipContent(true);
-//                    }
-//                });
+                .setApplicationName("funfSensor")
+                .setRootUrl("http://192.168.0.100:8080/_ah/api/")
+                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                    @Override
+                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                        abstractGoogleClientRequest.setDisableGZipContent(true);
+                    }
+                });
         return builder.build();
     }
+
+//    @Provides
+//    ProbeDataSetApi provideProbeDataSetApi() {
+//        ProbeDataSetApi.Builder builder = new ProbeDataSetApi.Builder(AndroidHttp.newCompatibleTransport(),
+//                new AndroidJsonFactory(), null)
+//                .setRootUrl("https://funfcese.appspot.com/_ah/api/");
+////                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+////                    @Override
+////                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+////                        abstractGoogleClientRequest.setDisableGZipContent(true);
+////                    }
+////                });
+//        return builder.build();
+//    }
 
 }
