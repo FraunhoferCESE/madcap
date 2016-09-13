79359a522c834007937782db06f3e7550883b561
Lucas Layman <llayman@fc-md.umd.edu>
2016-09-13 15:42:12 -0400

    Updated gradle to pull keystore configuration from a properties file. Added boileyplate constants file for backend

---
+++ backend/src/main/java/org/fraunhofer/cese/madcap/backend/Constants.java
@@ -0,0 +1,11 @@
+package org.fraunhofer.cese.madcap.backend;
+
+/**
+ * Created by llayman on 9/13/2016.
+ */
+public class Constants {
+
+    public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+    public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+}
