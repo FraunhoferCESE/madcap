8e328537282b3c16da37eb14118b35e6219ffe20
Lucas Layman <llayman@fc-md.umd.edu>
2016-09-15 15:14:30 -0400

    Added onEnable tests to BluetoothProbeTest

--- app/src/test/java/org/fraunhofer/cese/madcap/Probe/BluetoothProbeTest.java
+++ app/src/test/java/org/fraunhofer/cese/madcap/Probe/BluetoothProbeTest.java
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 
 import com.google.gson.JsonObject;
@@ -14,6 +15,7 @@ import org.fraunhofer.cese.madcap.JsonObjectFactory;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RuntimeEnvironment;
 
@@ -275,6 +277,105 @@ public class BluetoothProbeTest extends AbstractTest{
 
         when(mockBluetoothAdapter.getState()).thenReturn(BluetoothAdapter.ERROR);
         Assert.assertEquals("Unmachted return values", Integer.toString(mockBluetoothAdapter.getState()), bluetoothProbe.getBluetoothState());
+    }
+
+
+    /**
+     * Test the case where there is no bluetooth adapter during onEnable
+     */
+    @Test
+    public void testOnEnableNoAdapter() {
+        Context mockContext = mock(Context.class);
+        JsonObjectFactory mockJsonObjectFactory = mock(JsonObjectFactory.class);
+        when(mockJsonObjectFactory.createJsonObject(any(Intent.class))).thenReturn(new JsonObject());
+
+        // Run the CUT code without passing in a BluetoothAdapter
+        BluetoothProbe bluetoothProbe = new BluetoothProbe(null, mockContext, mockJsonObjectFactory, null);
+        bluetoothProbe.onEnable();
+
+        // bluetoothAdapter == null. No data should be sent to the jsonObjectFactory.
+        verify(mockJsonObjectFactory, never()).createJsonObject(any(Intent.class));
+
+    }
+
+    /**
+     * Test the case where there are no bonded devices in onEnable
+     */
+    @Test
+    public void testOnEnableAdapterNoDevices() {
+
+        Context mockContext = mock(Context.class);
+        JsonObjectFactory mockJsonObjectFactory = mock(JsonObjectFactory.class);
+        when(mockJsonObjectFactory.createJsonObject(any(Intent.class))).thenReturn(new JsonObject());
+
+
+        // Set up how our BluetoothAdapter will respnd to methods called during onEnable()...
+        BluetoothAdapter mockBluetoothAdapter = mock(BluetoothAdapter.class);
+        when(mockBluetoothAdapter.getState()).thenReturn(BluetoothAdapter.STATE_OFF);
+        when(mockBluetoothAdapter.getAddress()).thenReturn("127.0.0.1");
+        when(mockBluetoothAdapter.getName()).thenReturn("this is my mock adapter!");
+        when(mockBluetoothAdapter.getBondedDevices()).thenReturn(new HashSet<BluetoothDevice>());
+
+        // Run the CUT code.
+        BluetoothProbe probe = new BluetoothProbe(mockBluetoothAdapter, mockContext, mockJsonObjectFactory, null);
+        probe.onEnable();
+
+        // bluetoothAdapter != null. Many things should be called. Check everything that is visible to us.
+        verify(mockContext, times(1)).registerReceiver(any(BluetoothInformationReceiver.class), any(IntentFilter.class));
+        verify(mockBluetoothAdapter, times(1)).getBondedDevices();
+
+        // Verify that createJsonObject was called and inspect the Intent that was passed. The contents should match what we told the mockBluetoothAdapter to return.
+        ArgumentCaptor<Intent> argument = ArgumentCaptor.forClass(Intent.class);
+        verify(mockJsonObjectFactory).createJsonObject(argument.capture());
+        assertEquals("Initial Probe!", argument.getValue().getStringExtra("BluetoothProbe: "));
+        assertEquals("OFF", argument.getValue().getStringExtra("State: "));
+        assertEquals("127.0.0.1", argument.getValue().getStringExtra("Address: "));
+        assertEquals("this is my mock adapter!", argument.getValue().getStringExtra("Name: "));
+        assertEquals("[]", argument.getValue().getStringExtra("Bonded devices: ")); // this is what an empty list.toString looks like
+    }
+
+    /**
+     * Test the case where there are some devices in onEnable
+     */
+    @Test
+    public void testOnEnableAdapterSomeDevices() {
+
+        Context mockContext = mock(Context.class);
+        JsonObjectFactory mockJsonObjectFactory = mock(JsonObjectFactory.class);
+        when(mockJsonObjectFactory.createJsonObject(any(Intent.class))).thenReturn(new JsonObject());
+
+        // Make some mock BluetoothDevices to return
+        BluetoothDevice mockBluetoothDevice1 = mock(BluetoothDevice.class);
+        when(mockBluetoothDevice1.getName()).thenReturn("Ear piece");
+        BluetoothDevice mockBluetoothDevice2 = mock(BluetoothDevice.class);
+        when(mockBluetoothDevice2.getName()).thenReturn("speaker");
+        Set<BluetoothDevice> bluetoothDevices = new HashSet<>();
+        bluetoothDevices.add(mockBluetoothDevice1);
+        bluetoothDevices.add(mockBluetoothDevice2);
+
+        // Set up how our BluetoothAdapter will respnd to methods called during onEnable()...
+        BluetoothAdapter mockBluetoothAdapter = mock(BluetoothAdapter.class);
+        when(mockBluetoothAdapter.getState()).thenReturn(BluetoothAdapter.STATE_OFF);
+        when(mockBluetoothAdapter.getAddress()).thenReturn("127.0.0.1");
+        when(mockBluetoothAdapter.getName()).thenReturn("this is my mock adapter!");
+        when(mockBluetoothAdapter.getBondedDevices()).thenReturn(bluetoothDevices);
+
+        // Run the CUT code.
+        BluetoothProbe probe = new BluetoothProbe(mockBluetoothAdapter, mockContext, mockJsonObjectFactory, null);
+        probe.onEnable();
+
+        // bluetoothAdapter != null. Many things should be called. Check everything that is visible to us.
+        verify(mockContext, times(1)).registerReceiver(any(BluetoothInformationReceiver.class), any(IntentFilter.class));
+        verify(mockBluetoothAdapter, times(1)).getBondedDevices();
+
+        // Verify that createJsonObject was called and inspect the Intent that was passed. The contents should match what we told the mockBluetoothAdapter to return.
+        ArgumentCaptor<Intent> argument = ArgumentCaptor.forClass(Intent.class);
+        verify(mockJsonObjectFactory).createJsonObject(argument.capture());
+        assertEquals("Initial Probe!", argument.getValue().getStringExtra("BluetoothProbe: "));
+        assertEquals("OFF", argument.getValue().getStringExtra("State: "));
+        assertEquals("127.0.0.1", argument.getValue().getStringExtra("Address: "));
+        assertEquals("this is my mock adapter!", argument.getValue().getStringExtra("Name: "));
+        assertEquals("[Ear piece, speaker]", argument.getValue().getStringExtra("Bonded devices: ")); // this is what an empty list.toString looks like
     }
 
 }
