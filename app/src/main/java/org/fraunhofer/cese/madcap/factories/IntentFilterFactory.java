05db35a80ed635ee5304ce1531608eb71c1c0eb9
Lucas Layman <llayman@fc-md.umd.edu>
2016-09-25 20:06:15 -0400

    Updated reference to Apache Commons Factory

--- app/src/main/java/org/fraunhofer/cese/madcap/factories/IntentFilterFactory.java
+++ app/src/main/java/org/fraunhofer/cese/madcap/factories/IntentFilterFactory.java
@@ -2,7 +2,8 @@ package org.fraunhofer.cese.madcap.factories;
 
 import android.content.IntentFilter;
 
-import org.apache.commons.collections.Factory;
+import org.apache.commons.collections4.Factory;
+
 
 /**
  * Created by MMueller on 9/21/2016.
