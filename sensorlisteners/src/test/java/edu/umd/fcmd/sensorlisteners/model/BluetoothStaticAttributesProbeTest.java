a3586f98cb3279f838c2e21306924174762ba8b5
FCMD\MMueller <MMueller@fc-md.umd.edu>
2016-12-07 17:06:07 -0500

    Added BluetoothStaticAttributesProbeTest.

---
+++ sensorlisteners/src/test/java/edu/umd/fcmd/sensorlisteners/model/BluetoothStaticAttributesProbeTest.java
@@ -0,0 +1,82 @@
+package edu.umd.fcmd.sensorlisteners.model;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static android.R.attr.name;
+import static org.junit.Assert.*;
+
+/**
+ * Created by MMueller on 12/7/2016.
+ */
+public class BluetoothStaticAttributesProbeTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+        BluetoothStaticAttributesProbe cut = new BluetoothStaticAttributesProbe();
+
+        cut.setName("name");
+
+        Assert.assertEquals(cut.getName(), "name");
+    }
+
+    @Test
+    public void setName() throws Exception {
+        BluetoothStaticAttributesProbe cut = new BluetoothStaticAttributesProbe();
+
+        cut.setName("name");
+
+        Assert.assertEquals(cut.getName(), "name");
+    }
+
+    @Test
+    public void getAddress() throws Exception {
+        BluetoothStaticAttributesProbe cut = new BluetoothStaticAttributesProbe();
+
+        cut.setAddress("address");
+
+        Assert.assertEquals(cut.getAddress(), "address");
+    }
+
+    @Test
+    public void setAddress() throws Exception {
+        BluetoothStaticAttributesProbe cut = new BluetoothStaticAttributesProbe();
+
+        cut.setAddress("address");
+
+        Assert.assertEquals(cut.getAddress(), "address");
+    }
+
+    @Test
+    public void getType() throws Exception {
+        BluetoothStaticAttributesProbe cut = new BluetoothStaticAttributesProbe();
+
+        Assert.assertEquals(cut.getType(), "BluetoothStaticAttributes");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        BluetoothStaticAttributesProbe cut = new BluetoothStaticAttributesProbe();
+
+        cut.setName("name");
+        cut.setAddress("address");
+
+        Assert.assertEquals(cut.toString(),"{\"name\": " + "name" +
+                ", \"address\": " + "\"" + "address" + "\"" +
+                '}' );
+
+    }
+
+}
