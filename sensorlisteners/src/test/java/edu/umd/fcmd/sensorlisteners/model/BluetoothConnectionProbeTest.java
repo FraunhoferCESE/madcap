1ef137b6ef85cc28984224dd89e957db89e2de9b
FCMD\MMueller <MMueller@fc-md.umd.edu>
2016-12-07 16:33:23 -0500

    Added BluetoothConnectionProbeTest.

---
+++ sensorlisteners/src/test/java/edu/umd/fcmd/sensorlisteners/model/BluetoothConnectionProbeTest.java
@@ -0,0 +1,102 @@
+package edu.umd.fcmd.sensorlisteners.model;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MMueller on 12/7/2016.
+ */
+public class BluetoothConnectionProbeTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getState() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setState("A");
+
+        Assert.assertEquals(cut.getState(), "A");
+    }
+
+    @Test
+    public void setState() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setState("A");
+
+        Assert.assertEquals(cut.getState(), "A");
+    }
+
+    @Test
+    public void getForeignAddress() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setForeignName("name");
+
+        Assert.assertEquals(cut.getForeignName(), "name");
+    }
+
+    @Test
+    public void setForeignAddress() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setForeignAddress("name");
+
+        Assert.assertEquals(cut.getForeignAddress(), "name");
+    }
+
+    @Test
+    public void getForeignName() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setForeignAddress("name");
+
+        Assert.assertEquals(cut.getForeignAddress(), "name");
+    }
+
+    @Test
+    public void setForeignName() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setForeignAddress("name");
+
+        Assert.assertEquals(cut.getForeignAddress(), "name");
+    }
+
+    @Test
+    public void getType() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        Assert.assertEquals(cut.getType(), "BluetoothConnection");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        BluetoothConnectionProbe cut = new BluetoothConnectionProbe();
+
+        cut.setState("state");
+        cut.setForeignAddress("foreignAddress");
+        cut.setForeignName("foreignName");
+
+        Assert.assertEquals(cut.toString(), "{\"state\": " + "state" +
+                ", \"foreignName\": " + "foreignName" +
+                ", \"foreignAddress\": " + "\""+"foreignAddress"+"\"" +
+                '}');
+
+    }
+
+}
