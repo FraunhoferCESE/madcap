1fd28815b1ea7c9c05d1d0d4302b66202b5575f5
Marcel Mueller <mmueller@fc-md.umd.edu>
2017-01-03 15:15:39 -0500

    #126 added UserPresenceProbe.

---
+++ sensorlisteners/src/main/java/edu/umd/fcmd/sensorlisteners/model/UserPresenceProbe.java
@@ -0,0 +1,63 @@
+package edu.umd.fcmd.sensorlisteners.model;
+
+/**
+ * Created by MMueller on 1/3/2017.
+ */
+
+public class UserPresenceProbe extends Probe {
+    private String presence;
+
+
+    /**
+     * Gets the presence.
+     * @return the presence.
+     */
+    public String getPresence() {
+        return presence;
+    }
+
+    /**
+     * Sets the presence.
+     * @param presence the presence to be set.
+     */
+    public void setPresence(String presence) {
+        this.presence = presence;
+    }
+
+    /**
+     * Gets the type of an state e.g. Accelerometer
+     *
+     * @return the type of state.
+     */
+    @Override
+    public String getType() {
+        return "UserPresence";
+    }
+
+    /**
+     * Returns a string representation of the object. In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return "{\"presence\": " + (presence != null? presence : "-") +
+                '}';
+    }
+}
