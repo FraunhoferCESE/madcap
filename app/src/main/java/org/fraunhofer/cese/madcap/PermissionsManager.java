63f2f62f186b6bfb05e32bc1b4fb7fa0675f3dec
Pramod <pdg.august@gmail.com>
2017-04-25 14:43:38 -0400

    Permissions handled through permission manager

--- app/src/main/java/org/fraunhofer/cese/madcap/PermissionsManager.java
+++ app/src/main/java/org/fraunhofer/cese/madcap/PermissionsManager.java
@@ -3,8 +3,10 @@ package org.fraunhofer.cese.madcap;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.AppOpsManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -20,7 +22,7 @@ import android.widget.CompoundButton;
 
 public class PermissionsManager extends Activity implements CheckBox.OnCheckedChangeListener{
     private static final int REQUEST_CODE = 998;
-    private boolean ACCESS_PERMIT = false;
+//    private boolean ACCESS_PERMIT = false;
 
     CheckBox contactsCB, locationCB, storageCB;
     CheckBox smsCB, telephoneCB, usageStatsCB;
@@ -34,44 +36,49 @@ public class PermissionsManager extends Activity implements CheckBox.OnCheckedCh
         setContentView(R.layout.activity_permissions);
 
         contactsCB = (CheckBox) findViewById(R.id.contactsCheckBox);
+        if(isContactPermitted()){
+            contactsCB.setChecked(true); contactsCB.setClickable(false); contactsCB.setEnabled(false);
+        }
         contactsCB.setOnCheckedChangeListener(this);
-        contactsCB.setChecked(isContactPermitted());
-//                (ActivityCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED) ? true :
-//                        getPermissionWithRationale(Manifest.permission.GET_ACCOUNTS, "MADCAP needs to access your Google username and email. It does not read your other contacts."));
 
         locationCB = (CheckBox) findViewById(R.id.locationCheckBox);
+        if(isLocationPermitted()){
+            locationCB.setChecked(true); locationCB.setClickable(false); locationCB.setEnabled(false);
+        }
         locationCB.setOnCheckedChangeListener(this);
-        locationCB.setChecked(isLocationPermitted());
-//                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) ? true :
-//                        getPermissionWithRationale(Manifest.permission.ACCESS_FINE_LOCATION,"MADCAP requires permission to access your location."));
 
         storageCB = (CheckBox) findViewById(R.id.storageCheckBox);
+        if(isStoragePermitted()){
+            storageCB.setChecked(true); storageCB.setClickable(false); storageCB.setEnabled(false);
+        }
         storageCB.setOnCheckedChangeListener(this);
-        storageCB.setChecked(isStoragePermitted());
-//                (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) ? true :
-//                        getPermissionWithRationale(Manifest.permission.READ_EXTERNAL_STORAGE, "MADCAP requires your permission to read and write data to your phone's memory."));
+
 
         smsCB = (CheckBox) findViewById(R.id.smsCheckBox);
+        if(isSmsPermitted()){
+            smsCB.setChecked(true); smsCB.setClickable(false); smsCB.setEnabled(false);
+        }
         smsCB.setOnCheckedChangeListener(this);
-        smsCB.setChecked(isSmsPermitted());
-//                (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) ? true :
-//                        getPermissionWithRationale(Manifest.permission.READ_SMS, "MADCAP needs this permission to know when you receive text messages. It does not read your texts."));
+
 
         telephoneCB = (CheckBox) findViewById(R.id.telephoneCheckBox);
+        if(isTelephonePermitted()){
+            telephoneCB.setChecked(true); telephoneCB.setClickable(false); telephoneCB.setEnabled(false);
+        }
         telephoneCB.setOnCheckedChangeListener(this);
-        telephoneCB.setChecked(isTelephonePermitted());
-//                (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) ? true :
-//                        getPermissionWithRationale(Manifest.permission.READ_PHONE_STATE, "MADCAP needs this permission to know when you are making or receiving phone calls."));
+
 
         usageStatsCB = (CheckBox) findViewById(R.id.usageStatsCheckBox);
+        if(isUsageStatsPermitted()){
+            usageStatsCB.setChecked(true); usageStatsCB.setClickable(false); usageStatsCB.setEnabled(false);
+        }
         usageStatsCB.setOnCheckedChangeListener(this);
-        usageStatsCB.setChecked(isUsageStatsPermitted());
 
     }
 
-    private boolean getPermissionWithRationale(String permit, String rationale) {
+    private void getPermissionWithRationale(String permit, String rationale) {
         dialogBuilder = new AlertDialog.Builder(this);
-        ACCESS_PERMIT = false;
+//        ACCESS_PERMIT = false;
         switch (permit) {
             case Manifest.permission.GET_ACCOUNTS:
                 dialogBuilder.setTitle("MADCAP permissions")
@@ -168,6 +175,11 @@ public class PermissionsManager extends Activity implements CheckBox.OnCheckedCh
                                         Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(intent);
+                                        if(isUsageStatsPermitted()){
+                                            usageStatsCB.setChecked(true);
+                                            usageStatsCB.setClickable(false);
+                                            usageStatsCB.setEnabled(false);
+                                        }
                                         dialog.dismiss();
                                     }
                                 })
@@ -176,7 +188,7 @@ public class PermissionsManager extends Activity implements CheckBox.OnCheckedCh
                 dialog.show();
                 break;
         }
-        return ACCESS_PERMIT;
+//        return ACCESS_PERMIT;
     }
 
     @Override
@@ -185,17 +197,17 @@ public class PermissionsManager extends Activity implements CheckBox.OnCheckedCh
         if((grantResults[0] == PackageManager.PERMISSION_GRANTED))
             switch(requestCode){
                 case REQUEST_CODE:
-                    contactsCB.setChecked(true); contactsCB.setClickable(false); break;
-                case REQUEST_CODE-3:
-                    smsCB.setChecked(true); smsCB.setClickable(false); break;
+                    contactsCB.setChecked(true); contactsCB.setClickable(false); contactsCB.setEnabled(false); break;
                 case REQUEST_CODE-1:
-                    locationCB.setChecked(true); locationCB.setClickable(false); break;
+                    locationCB.setChecked(true); locationCB.setClickable(false); locationCB.setEnabled(false); break;
                 case REQUEST_CODE-2:
-                    storageCB.setChecked(true); storageCB.setClickable(false); break;
+                    storageCB.setChecked(true); storageCB.setClickable(false); storageCB.setEnabled(false); break;
+                case REQUEST_CODE-3:
+                    smsCB.setChecked(true); smsCB.setClickable(false); smsCB.setEnabled(false); break;
                 case REQUEST_CODE-4:
-                    telephoneCB.setChecked(true); telephoneCB.setClickable(false); break;
-                case REQUEST_CODE-5:
-                    usageStatsCB.setChecked(true); usageStatsCB.setClickable(false); break;
+                    telephoneCB.setChecked(true); telephoneCB.setClickable(false); telephoneCB.setEnabled(false); break;
+//                case REQUEST_CODE-5:
+//                    usageStatsCB.setChecked(true); usageStatsCB.setClickable(false); usageStatsCB.setEnabled(false); break;
             }
     }
 
@@ -219,36 +231,42 @@ public class PermissionsManager extends Activity implements CheckBox.OnCheckedCh
                 getPermissionWithRationale(Manifest.permission.READ_PHONE_STATE, getString(R.string.telephone_rationale)); return;
             case R.id.usageStatsCheckBox:
 //                if(!isChecked) return;
-                getPermissionWithRationale(Settings.ACTION_USAGE_ACCESS_SETTINGS, getString(R.string.access_usage_rationale));
+                getPermissionWithRationale(Settings.ACTION_USAGE_ACCESS_SETTINGS, getString(R.string.access_usage_rationale)); return;
 
         }
     }
 
     public boolean isContactPermitted(){
-        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED) ? true : false;
+        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED); //? true : false;
     }
 
     public boolean isLocationPermitted(){
-        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) ? true : false;
+        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);// ? true : false;
     }
 
     public boolean isSmsPermitted(){
-        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) ? true : false;
+        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED);// ? true : false;
     }
 
     public boolean isStoragePermitted(){
-        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) ? true : false;
+        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);// ? true : false;
     }
 
     public boolean isTelephonePermitted(){
-        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) ? true : false;
+        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED);// ? true : false;
     }
 
     public boolean isUsageStatsPermitted(){
-        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Settings.ACTION_USAGE_ACCESS_SETTINGS) == PackageManager.PERMISSION_GRANTED) ? true : false;
-    }
+//        return (ActivityCompat.checkSelfPermission(PermissionsManager.this, Settings.ACTION_USAGE_ACCESS_SETTINGS) == PackageManager.PERMISSION_GRANTED) ? true : false;
+        try {
+            ApplicationInfo appInfo = getPackageManager().getApplicationInfo(getPackageName(),0);
+            AppOpsManager appOpsManager = (AppOpsManager) getSystemService(APP_OPS_SERVICE);
+            int mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, appInfo.uid, appInfo.packageName);
+            return (mode == AppOpsManager.MODE_ALLOWED);
 
-//    public boolean isPermitted(String permissionString){
-//        switch
-//    }
+        } catch (PackageManager.NameNotFoundException e) {
+            //do not resolve this catch.
+            return false;
+        }
+    }
 }
